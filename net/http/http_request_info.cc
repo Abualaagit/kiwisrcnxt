0de133f35a49527cf242183687c0591dcd602c03
Samo <88598440+Abualaagit@users.noreply.github.com>
2022-12-07 01:28:54 +0300

    Initialize chromium

---
+++ net/http/http_request_info.cc
@@ -0,0 +1,17 @@
+// Copyright 2010 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "net/http/http_request_info.h"
+
+#include "net/dns/public/secure_dns_policy.h"
+
+namespace net {
+
+HttpRequestInfo::HttpRequestInfo() = default;
+
+HttpRequestInfo::HttpRequestInfo(const HttpRequestInfo& other) = default;
+
+HttpRequestInfo::~HttpRequestInfo() = default;
+
+}  // namespace net
